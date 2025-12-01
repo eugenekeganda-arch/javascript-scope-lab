d26207ea88940efd0f1d5312bc1d15c0645b3c11
Eugene <eugenekeganda@icloud.com>
2025-12-01 14:15:26 +0300

    Flat bugger

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+// global variables
+let burgers = ['hamburger', 'cheeseburgeer'];
+let featuredDrink = 'Strawberry Milkshake';
+
+
+//the add burger function
+function addBurger() {
+    var newBurger = 'Flatbuger';
+    burgers.push(newBurger);
+
+}
+addBurger();
+console.log(burgers);
+
+
+//the block scope
+if (true) {
+    let anothernewBurger = 'Maple Bacon Burger';
+    burgers.push(anothernewBurger);
+
+    function changefeaturedDrink(){
+        featuredDrink = 'The Javashake';
+    }
+
+    changefeaturedDrink();
+    console.log(featuredDrink);
+         
+
+}
+
+
